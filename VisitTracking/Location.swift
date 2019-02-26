9e22e609b66b6fc21fa292e033005505ed770b4e
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-02-26 09:59:45 +0800

    added map controller

---
+++ VisitTracking/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  VisitTracking
+//
+//  Created by Ankit Kumar on 25/02/2019.
+//  Copyright © 2019 Ankit Kumar. All rights reserved.
+//
+
+import Foundation
+class Location : Codable {
+
+let latitude: Double
+let longitude: Double
+let date: Date
+let dateString: String
+let description: String
+    
+}
