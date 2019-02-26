4b7276437cd898bb2f67622bfd3d8f49c67ba520
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-02-26 10:27:53 +0800

    created notification content for newly visited location

--- VisitTracking/AppDelegate.swift
+++ VisitTracking/AppDelegate.swift
@@ -17,11 +17,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     let center = UNUserNotificationCenter.current()
     let locationManager = CLLocationManager()
-
+    static let geoCoder = CLGeocoder()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+    
+        let tabTintColor = UIColor(red: 0/255, green: 104/255, blue: 55/255, alpha: 1)
+        UITabBar.appearance().tintColor = tabTintColor
+        
+        //Asking for Notifications Permissions
+        center.requestAuthorization(options: [.alert, .sound]) { granted, error in
+        }
+        
+        //Asking for location permission
+        locationManager.requestAlwaysAuthorization()
+        
+        //Subscribe to Location Changes
+        locationManager.startMonitoringVisits()
+        locationManager.delegate = self
+        
+        // 1
+        locationManager.distanceFilter = 35
+        
+        // 2
+        locationManager.allowsBackgroundLocationUpdates = true
+        
+        // 3
+        locationManager.startUpdatingLocation()
+        
+        
         return true
     }
 
@@ -48,5 +75,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+}
+
+
+// Core Location uses delegate callbacks to inform you of location changes
+extension AppDelegate: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didVisit visit: CLVisit) {
+        // create CLLocation from the coordinates of CLVisit
+        let clLocation = CLLocation(latitude: visit.coordinate.latitude, longitude: visit.coordinate.longitude)
+        
+        // Get location description
+    }
+    
+    func newVisitReceived(_ visit: CLVisit, description: String) {
+        let location = Location(visit: visit, descriptionString: description)
+        
+        // Save location to disk
+        LocationStorage.shared.saveLocationOnDisk(location)
+        
+        // create notification content
+        let content = UNMutableNotificationContent()
+        content.title = "New Place Visited"
+        content.body = location.description
+        content.sound = .default
+        
+        // Create a one second long trigger and notification request with that trigger.
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        let request = UNNotificationRequest(identifier: location.dateString, content: content, trigger: trigger)
+        
+        // Schedule the notification by adding the request to notification center.
+        center.add(request, withCompletionHandler: nil)
+        
+    }
 }
 
