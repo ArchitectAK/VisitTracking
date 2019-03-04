803d962ef0766545d96fba93877f93f7a913dc85
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-03-04 18:24:15 +0800

    updated maps

--- VisitTracking/MapViewController.swift
+++ VisitTracking/MapViewController.swift
@@ -10,21 +10,44 @@ import UIKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        mapView.userTrackingMode = .follow
+        
+        let annotations = LocationsStorage.shared.locations.map { annotationForLocation($0) }
+        mapView.addAnnotations(annotations)
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(newLocationAdded(_:)),
+            name: .newLocationSaved,
+            object: nil)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addItemPressed(_ sender: Any) {
+        guard let currentLocation = mapView.userLocation.location else {
+            return
+        }
+        
+        LocationsStorage.shared.saveCLLocationToDisk(currentLocation)
     }
-    */
+    
+    func annotationForLocation(_ location: Location) -> MKAnnotation {
+        let annotation = MKPointAnnotation()
+        annotation.title = location.dateString
+        annotation.coordinate = location.coordinates
+        return annotation
+    }
+    @objc func newLocationAdded(_ notification: Notification) {
+        guard let location = notification.userInfo?["location"] as? Location else {
+            return
+        }
+        
+        let annotation = annotationForLocation(location)
+        mapView.addAnnotation(annotation)
+    }
 
 }
